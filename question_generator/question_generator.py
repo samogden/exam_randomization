c4c074a44f7e7e965c0fb9e5e24ad39a168cf9e1
Sam O <2274654+samogden@users.noreply.github.com>
2024-04-10 12:57:38 -0700

    Changing how generation is done to be more class-oriented

--- question_generator/question_generator.py
+++ question_generator/question_generator.py
@@ -2,6 +2,7 @@
 import argparse
 import datetime
 import random
+import subprocess
 import sys
 import time
 from typing import List
@@ -10,6 +11,9 @@ import textwrap
 
 import inspect
 import importlib
+
+import text2qti.quiz
+
 import math_questions
 
 
@@ -32,27 +36,30 @@ def generate_quiz(name:str, modules:List[str], num_variations_per_class=1, group
   def get_classes(module):
     return [obj for name, obj in inspect.getmembers(module) if inspect.isclass(obj) and obj.__module__ == module.__name__]
   
-  
-  with open('-'.join(name.split(' ')) + ".md", 'w') as fid:
     
-    fid.write(
-      textwrap.dedent(
-        f"""
-        Quiz title: {name}
-        Quiz description: trying out some new generation things
-        
-        """
-      )
-    )
+  markdown_text = textwrap.dedent(
+    f"""
+      Quiz title: {name}
+      
+      """
+  )
   
-    for module in modules:
-      m = importlib.import_module(module)
-      for c in get_classes(m):
-        if "MemoryAccessQuestion" in c.__name__: continue # todo: fix this hack
-        logging.debug(c)
-        fid.write(c.generate_group_markdown(num_variations=num_variations_per_class, points_per_question=2))
-        fid.write("\n\n")
-
+  for module in modules:
+    m = importlib.import_module(module)
+    for c in get_classes(m):
+      if "MemoryAccessQuestion" in c.__name__: continue # todo: fix this hack
+      logging.debug(c)
+      markdown_text += c.generate_group_markdown(num_variations=num_variations_per_class, points_per_question=2)
+      markdown_text += "\n\n"
+      # fid.write(c.generate_group_markdown(num_variations=num_variations_per_class, points_per_question=2))
+      # fid.write("\n\n")
+  
+  markdown_file_name = '-'.join(name.split(' ')) + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + ".md"
+  with open(markdown_file_name, 'w') as fid:
+    fid.write(markdown_text)
+  
+  return markdown_file_name
+  
 
 def main():
   
@@ -64,7 +71,8 @@ def main():
     # "process_questions"
   ]
   
-  generate_quiz("Mixed Quiz", modules)
+  markdown_file = generate_quiz("Mixed Quiz", modules, num_variations_per_class=flags.num_variations)
+  subprocess.Popen(f"text2qti {markdown_file}", shell=True)
   
   return
 
