9156666097f991b513588af5e83c1881f65265eb
Sam O <2274654+samogden@users.noreply.github.com>
2024-04-18 12:03:53 -0700

    Updating to have a better way of specifying how many variations to pick per class, as well as classes to pick from

--- question_generator/question_generator.py
+++ question_generator/question_generator.py
@@ -31,32 +31,34 @@ def get_flags():
   
 
 
-def generate_quiz(name:str, modules:List[str], num_variations_per_class=1, group_variations=True, allowed_classes=None):
+def generate_quiz(quiz_name:str, modules:List[str], num_variations_per_class=1, group_variations=True, question_classes=None, **kwargs):
+  
   
   def get_classes(module):
     logging.debug([name for name, obj in inspect.getmembers(module) if inspect.isclass(obj) and obj.__module__ == module.__name__])
-    return [obj for name, obj in inspect.getmembers(module) if inspect.isclass(obj) and obj.__module__ == module.__name__ and (allowed_classes is not None and name in allowed_classes)]
+    return [(obj, name) for name, obj in inspect.getmembers(module) if inspect.isclass(obj) and obj.__module__ == module.__name__ and (question_classes is not None and name in question_classes)]
   
-    
+  generation_time = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
   markdown_text = textwrap.dedent(
     f"""
-      Quiz title: {name}
+      Quiz title: {quiz_name}-{generation_time}
       
       """
   )
   
   for module in modules:
     m = importlib.import_module(module)
-    for c in get_classes(m):
-      if "MemoryAccessQuestion" in c.__name__: continue # todo: fix this hack
+    for (c, name) in get_classes(m):
       
       logging.debug(c)
-      markdown_text += c.generate_group_markdown(num_variations=num_variations_per_class, points_per_question=2)
+      markdown_text += c.generate_group_markdown(
+        num_variations=num_variations_per_class,
+        points_per_question=2,
+        num_to_pick=question_classes[name]
+      )
       markdown_text += "\n\n"
-      # fid.write(c.generate_group_markdown(num_variations=num_variations_per_class, points_per_question=2))
-      # fid.write("\n\n")
   
-  markdown_file_name = '-'.join(name.split(' ')) + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + ".md"
+  markdown_file_name = '-'.join(quiz_name.split(' ')) + "-" + generation_time + ".md"
   with open(markdown_file_name, 'w') as fid:
     fid.write(markdown_text)
   
@@ -74,11 +76,11 @@ def main():
   ]
   
   markdown_file = generate_quiz("Mixed Quiz", modules, num_variations_per_class=flags.num_variations,
-    allowed_classes=[
-      "BaseAndBounds",
-      "Paging_with_table",
-      "SchedulingQuestion"
-    ]
+    question_classes = {
+      "BaseAndBounds" : 1,
+      "Paging_with_table" : 1,
+      "SchedulingQuestion" : 2,
+    }
   )
   subprocess.Popen(f"text2qti {markdown_file}", shell=True)
   
