871f8067a99f2fdc686906d7dabf038df5ff0d13
samogden <samuel.s.ogden@gmail.com>
2024-11-16 09:10:35 -0800

    Adding in inode access question

--- question_generator/persistance_questions.py
+++ question_generator/persistance_questions.py
@@ -17,7 +17,6 @@ class HardDriveAccessTime(CanvasQuestion):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     
-    
     # Given:
     # 1. hard drive rotation speed
     # 2. seek time
@@ -66,7 +65,6 @@ class HardDriveAccessTime(CanvasQuestion):
       "Given the information below, please calculate the following values.  Make sure your answers are rounded to 2 decimal points (even if they are whole numbers)."
       ]
     
-    
     lines.extend(
       self.get_table_lines(
         table_data={
@@ -79,7 +77,6 @@ class HardDriveAccessTime(CanvasQuestion):
         add_header_space=True
       )
     )
-    
     
     lines.extend(
       self.get_table_lines(
@@ -124,7 +121,97 @@ class HardDriveAccessTime(CanvasQuestion):
     
     lines.extend([
       "Putting these together we see:",
-      "$$ " + f"t_{{total}} = (# reads) \\cdot t_{{access}} + t_{{transfer}} = {self.number_of_reads} \cdot {self.access_delay:0.2f} + {self.transfer_delay:0.2f} = {self.disk_access_delay:0.2f}ms" + " $$"
+      "$$ " + f"t_{{total}} = (# reads) \\cdot t_{{access}} + t_{{transfer}} = {self.number_of_reads} \\cdot {self.access_delay:0.2f} + {self.transfer_delay:0.2f} = {self.disk_access_delay:0.2f}ms" + " $$"
     ])
+    return lines
+
+
+
+class HardDriveAccessTime(CanvasQuestion):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    
+    # Given:
+    # 1. block size
+    # 2. inode number
+    # 3. inode start location
+    # 4. inode size
+    
+    # Calculate
+    # 1. inode address
+    # 1. block containing inode
+    # 2. memory index into block
+    
+    self.block_size = 1024 * random.randint(1,10)
+    self.inode_number = random.randint(0,1024)
+    self.inode_start_location = self.block_size * random.randint(2, 5)
+    self.inode_size = 2**random.randint(6, int(math.log2(self.block_size)-2))
+    
+    self.inode_address = self.inode_start_location + self.inode_number * self.inode_size
+    self.inode_block = self.inode_address // self.block_size
+    self.inode_address_in_block = self.inode_address % self.block_size
+    self.inode_index_in_block = int(self.inode_address_in_block / self.inode_size)
+    
+    self.blank_vars.update({
+      "inode_address" : Variable("Inode Address", self.inode_address),
+      "inode_block" : Variable("Inode block", self.inode_block),
+      "inode_address_in_block" : Variable("Inode address in block", self.inode_address_in_block),
+      "inode_index_in_block" : Variable("inode index in block", self.inode_index_in_block),
+    })
+  
+  
+  def get_question_body(self, *args, **kwargs) -> List[str]:
+    lines = [
+      "Given the information below, please calculate the following values. (hint: they should all be round numbers)."
+    ]
+    
+    lines.extend(
+      self.get_table_lines(
+        table_data={
+          f"Block Size" : [f"{self.block_size} Bytes"],
+          f"Inode Number" : [f"{self.inode_number}"],
+          f"Inode Start Location" : [f"{self.inode_start_location} Bytes"],
+          f"Inode size" : [f"{self.inode_size} Bytes"],
+        },
+        add_header_space=True
+      )
+    )
+    
+    lines.extend(
+      self.get_table_lines(
+        headers=["Variable", "Value"],
+        table_data={
+          "Inode address": ["[inode_address] Bytes"],
+          "Block containing inode" : ["[inode_block]"],
+          "Inode address (offset) within block" : ["[inode_address_in_block] Bytes offset"],
+          "Inode index within block" : ["[inode_index_in_block]"]
+        },
+        sorted_keys=["Inode address", "Block containing inode", "Inode address (offset) within block", "Inode index within block"]
+      )
+    )
+    
+    return lines
+  
+  def get_explanation(self, *args, **kwargs) -> List[str]:
+    lines = []
+    
+    lines.extend([
+      "If we are given an inode number, there are a few steps that we need to take to load the actual inode.  These consist of determining the address of the inode, which block would contain it, and then its address within the block.",
+      ""
+      "To find the inode address, we calculate:",
+      r"$$ {addr}_{inode} = {addr}_{inode\_start} + (\text{inode#}) \cdot (\text{inode size}) = " + f"{self.inode_start_location} + {self.inode_number} \\cdot {self.inode_size} = {self.inode_address}" + " $$",
+      "",
+      "Next, we us this to figure out what block the inode is in.  We do this directly so we know what block to load, thus minimizing the number of loads we have to make."
+      r"$$ \text{block_to_load} = {addr}_{inode} \mathbin{//} (\text{block size}) = " + f"{self.inode_address} \mathbin{{//}} {self.block_size} = {self.inode_block}" + "$$",
+      "",
+      "When we load this block, we now have in our system memory (remember, blocks on the hard drive are effectively useless to us until they're in main memory!), the inode, so next we need to figure out where it is within that block."
+      "This means that we'll need to find the offset into this block.  We'll calculate this both as the offset in bytes, and also in number of inodes, since we can use array indexing.",
+      "",
+      r"$$ \text{offset within block} = {addr}_{inode} % (\text{block size}) = " + f"{self.inode_address} % {self.block_size} = {self.inode_address_in_block} Bytes offset" + "$$",
+      "and"
+      r"$$ \text{index within block} = \frac{\text{offset within block}}{\text{inode size}} = " + f"\\frac{{{self.inode_address_in_block}}}{{{self.inode_size}}} = {self.inode_index_in_block}" + "$$"
+    ])
+    
+    
     return lines
   
