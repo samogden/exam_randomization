f86490d0e696f7dd4dd7401a788706018e7a4314
samogden <samuel.s.ogden@gmail.com>
2024-12-06 16:33:32 -0800

    Fixing the bash script which was producing invalid files

--- scripts/generate_practice_yaml.sh
+++ scripts/generate_practice_yaml.sh
@@ -1,14 +1,17 @@
 #!env bash
 
-question_classes=$(grep register src/premade_questions/*.py -a1 | grep -v basic.py | grep class | sed 's/.*class //g' | sed 's/(.*//g')
+question_classes=$(grep register src/premade_questions/*.py -a1 | grep -v basic.py | grep class | sed 's/.*class //g' | sed 's/(.*//g' | sed 's/:.*//g')
 
 for class in $question_classes ; do
   echo "name: \"(Ungraded) $(echo $class | sed 's/Question//g')\""
-  echo "practice: True"
-  echo "question:"
+  echo "practice: False"
+  echo "questions:"
   echo "  1:"
-  echo "    $(echo $class | sed 's/Question//g')"
+  echo "    $(echo $class | sed 's/Question//g'):"
   echo "      class: $class"
   echo "      repeat: 5"
   echo "---"
 done > all_classes.yaml
+
+# A hack to get rid of those dashes at the end
+sed -i '' '$d' all_classes.yaml
